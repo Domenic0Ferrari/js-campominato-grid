ba3eb0a47ce4b273e451c5656318060674bdf072
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-21 15:33:23 +0200

    add number in the "cell"

--- assets/myscript.js
+++ assets/myscript.js
@@ -11,13 +11,26 @@ const eleGrid = document.querySelector('.grid');
 // Generare la griglia
 createGrid(100, eleGrid);
 
+// Applicare gli event listeners a tutte le celle della griglia
+
+const listCells = document.querySelectorAll('.cell');
+for (let i = 0; i < listCells.length; i++) {
+	const cell = listCells[i];
+	cell.addEventListener('click', colorCell);
+}
+
 
 
 
 /* FUNCTION DEFINITIONS */
 
 function createGrid(numCells, eleContainer) {
-	for (let i = 0; i < numCells; i++) {
-		eleContainer.innerHTML += '<div class="cell"></div>';
+	for (let i = 1; i <= numCells; i++) {
+		eleContainer.innerHTML += `<div class="cell"> ${i} </div>`
 	}
+}
+
+function colorCell() {
+    console.log(this);
+    this.classList.toggle('clicked');
 }
