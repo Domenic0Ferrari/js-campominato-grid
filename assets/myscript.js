e6173c2efc117a552f802789705af617b168d222
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-21 15:43:01 +0200

    add "button" to create a "grid"

--- assets/myscript.js
+++ assets/myscript.js
@@ -5,24 +5,28 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
+// Creare un bottone ed assegnare l'evento "click" e la funzione per creare la griglia
+
+let myButton = document.getElementById("play");
+myButton.addEventListener('click', complete);
+
 // Selezionare gli elementi del DOM necessari
-const eleGrid = document.querySelector('.grid');
+// const eleGrid = document.querySelector('.grid');
 
 // Generare la griglia
-createGrid(100, eleGrid);
+// createGrid(100, eleGrid);
 
-// Applicare gli event listeners a tutte le celle della griglia
+// Applicare gli event listeners a tutte le celle della griglia e stampare in console
 
-const listCells = document.querySelectorAll('.cell');
-for (let i = 0; i < listCells.length; i++) {
-	const cell = listCells[i];
-	cell.addEventListener('click', colorCell);
-}
-
-
+// const listCells = document.querySelectorAll('.cell');
+// for (let i = 0; i < listCells.length; i++) {
+// 	const cell = listCells[i];
+// 	cell.addEventListener('click', colorCell);
+// }
 
 
-/* FUNCTION DEFINITIONS */
+
+/* DEFINIRE LE FUNZIONI */
 
 function createGrid(numCells, eleContainer) {
 	for (let i = 1; i <= numCells; i++) {
@@ -33,4 +37,14 @@ function createGrid(numCells, eleContainer) {
 function colorCell() {
     console.log(this);
     this.classList.toggle('clicked');
+}
+
+function complete(){
+    const eleGrid = document.querySelector('.grid');
+    createGrid(100, eleGrid);
+    const listCells = document.querySelectorAll('.cell');
+for (let i = 0; i < listCells.length; i++) {
+	const cell = listCells[i];
+	cell.addEventListener('click', colorCell);
+}
 }
