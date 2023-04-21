8dc6ac000565ca669bc4c766998e11ad9ebc506e
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-21 15:20:47 +0200

    add .grid with .cell

--- assets/myscript.js
+++ assets/myscript.js
@@ -4,3 +4,20 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
+
+// Selezionare gli elementi del DOM necessari
+const eleGrid = document.querySelector('.grid');
+
+// Generare la griglia
+createGrid(100, eleGrid);
+
+
+
+
+/* FUNCTION DEFINITIONS */
+
+function createGrid(numCells, eleContainer) {
+	for (let i = 0; i < numCells; i++) {
+		eleContainer.innerHTML += '<div class="cell"></div>';
+	}
+}
