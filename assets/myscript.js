83fc7803f3031ec7df9e8a603a65f8dd965e5c83
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-25 19:19:24 +0200

    add "easy", "normal" and "hard" mode

--- assets/myscript.js
+++ assets/myscript.js
@@ -42,16 +42,27 @@ function colorCell() {
 
 function complete(){
     const eleGrid = document.querySelector('.grid');
-    createGrid(100, eleGrid);
+    const eleDifficulty = document.querySelector('#mode');
+    let value = eleDifficulty.options[eleDifficulty.selectedIndex].value;
+    console.log(value);
+
+    if (value == "easy") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add("grid_easy");
+        createGrid(100, eleGrid);
+    } else if (value == "normal") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add( "grid_medium");
+        createGrid(81, eleGrid);
+    } else if (value == "hard") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add("grid_hard");
+        createGrid(49, eleGrid);
+    }
+
     const listCells = document.querySelectorAll('.cell');
     for (let i = 0; i < listCells.length; i++) {
 	    const cell = listCells[i];
 	    cell.addEventListener('click', colorCell);
     }
 }
-
-// BONUS
-
-// Selezionare la difficoltà nel select
-// Collegare la difficoltà selezionata al bottone "play"
-// Stampare la griglia richiesta a seconda della difficoltà scelta
